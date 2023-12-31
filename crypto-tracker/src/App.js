9c8c58232b225e6362d7e71771b5a41d0d0cbd3d
Akash Ajith <akash.ajithk@gmail.com>
2023-12-31 20:28:21 +0530

    completed the header component

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,10 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+import Singlecoin from './pages/Singlecoin';
+import {makeStyles} from "@material-ui/core"
+
 
 function App() {
+  const useStyles = makeStyles(()=>({
+    App : {
+      backgroundColor:"#14161a",
+      color:"white",
+      minHeight:"100vh"
+    }
+  }));
+  
+  const classes = useStyles()
+
   return (
-    <div className="App">
-     Hello World
-    </div>
+
+
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header/>
+        <Routes>
+          <Route path='/' Component={Homepage} exact/>
+          <Route path='/coins/:id' Component={Singlecoin}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ crypto-tracker/src/components/Header.js
@@ -0,0 +1,59 @@
+import { AppBar, Toolbar, Typography, Container, Select, MenuItem, makeStyles, createTheme, ThemeProvider } from '@material-ui/core'
+import React, { useState } from 'react'
+import { useNavigate,useContext } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
+
+
+const useStyles=makeStyles(()=>({
+  title:{
+    flex : 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight:"bold",
+    cursor:"pointer"
+  }
+}))
+
+const darkTheme = createTheme({
+  palette:{
+    primary:{
+      main:"#fff"
+    },
+    type: "dark"
+  }
+})
+
+const Header = () => {
+
+  const classes = useStyles()
+  const navigate = useNavigate()
+
+  const {currency, setCurrency, symbol} = CryptoState
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography onClick={()=>navigate('/')} className={classes.title} variant='h6'>
+              Crypto Hunter
+            </Typography>
+            <Select variant='outlined' style={{
+              width: 100,
+              height: 40,
+              marginRight:15
+              }}
+              value={currency}
+              onChange={(e)=>setCurrency(e.target.value)}
+              >
+              <MenuItem value={'USD'}>USD </MenuItem>
+              <MenuItem value={'INR'}>INR </MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default Header
